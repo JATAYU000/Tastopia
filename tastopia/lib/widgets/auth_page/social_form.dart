fa6ccb9342f524f144e192e1ea438349e2c2251a
JATAYU000 <shrivaths44kunju@gmail.com>
2024-02-20 17:26:48 +0530

    Bulk: login,signup ,buttons animations etc

---
+++ tastopia/lib/widgets/auth_page/social_form.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SocialButton extends StatelessWidget {
+  final double screenHeight,screenWidth;
+  const SocialButton({super.key,required this.screenHeight,required this.screenWidth});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Column(children: [
+            Row(
+              children: [
+                Expanded(
+                  child: Divider(
+                    color: Colors.black.withOpacity(0.7),
+                    thickness: 1,
+                  ),
+                ),
+                Container(padding: new EdgeInsets.all(10.0),child: Text("OR", style: GoogleFonts.sora(color: Colors.black.withOpacity(0.7),fontSize: 13,fontWeight: FontWeight.w500),)),
+                Expanded(
+                  child: Divider(
+                    color: Colors.black.withOpacity(0.7),
+                   thickness: 1,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: screenHeight*0.065,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                
+                Container(
+                  width:screenWidth*0.08,
+                  height: screenHeight*0.036,
+                  child: Image(image: AssetImage("assets/images/Google.svg"),)
+                  ),
+                Container(
+                  width:screenWidth*0.08,
+                  height: screenHeight*0.036,
+                  child: Image(image: AssetImage("assets/images/Octicons-mark-github.svg"),)
+                  ),
+                Container(
+                  width:screenWidth*0.08,
+                  height: screenHeight*0.036,
+                  child: Image(image: AssetImage("assets/images/2021_Facebook_icon.svg"),)
+                  ),
+              ],
+            ),
+            ]);
+  }
+}
